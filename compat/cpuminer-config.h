8cac05bdd6de6bca7fd3cb1b12c27c79163fa4e9
djm34 <djm34@aliceadsl.fr>
2018-11-24 23:24:20 +0200

    bump cpuminer version number

--- compat/cpuminer-config.h
+++ compat/cpuminer-config.h
@@ -103,7 +103,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.3.5"
+#define PACKAGE_VERSION "1.4.0"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
